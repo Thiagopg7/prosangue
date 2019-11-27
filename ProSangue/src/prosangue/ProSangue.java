c004213d0e04de94f43cfbdc8d6c70c0926701a2
Kurlok <felipe.augum@gmail.com>
2019-11-26 23:40:53 -0300

    Começando o trabalho
    triste

--- ProSangue/src/prosangue/ProSangue.java
+++ ProSangue/src/prosangue/ProSangue.java
@@ -5,12 +5,11 @@
  */
 package prosangue;
 
+import static javafx.application.Application.launch;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
@@ -18,27 +17,15 @@ import javafx.stage.Stage;
  * @author thiag
  */
 public class ProSangue extends Application {
-    
+
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
     }
 
     /**
@@ -47,5 +34,5 @@ public class ProSangue extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
