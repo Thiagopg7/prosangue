6c22b968cad967eba7251f595323754ff4d27273
Thiagopg7 <thiagopgpr@gmail.com>
2019-11-27 15:36:06 -0300

    Tabela e botões da tabela

--- ProSangue/src/prosangue/CadastroPacienteController.java
+++ ProSangue/src/prosangue/CadastroPacienteController.java
@@ -1,76 +1,137 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package prosangue;
 
 import java.net.URL;
+import java.sql.Array;
+import java.util.Observable;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Region;
+import persistencia.DoadorBD;
 
-/**
- * FXML Controller class
- *
- * @author felip
- */
-public class CadastroPacienteController implements Initializable {
+public class CadastroPacienteController {
+
+    @FXML
+    private TextField textCodigo;
+
+    @FXML
+    private TextField textNome;
+
+    @FXML
+    private DatePicker pickerNascimento;
+
+    @FXML
+    private TextField textPai;
+
+    @FXML
+    private TextField textMae;
+
+    @FXML
+    private TextField textRG;
 
     @FXML
     private Button buscaDoador;
+
     @FXML
     private Button btnCadastrar;
+
     @FXML
     private Button btnAlterar;
+
     @FXML
     private Button btnExcluir;
+
     @FXML
     private Button btnCancelar;
+
     @FXML
-    private TextField textCodigo;
-    @FXML
-    private TextField textNome;
-    @FXML
-    private DatePicker pickerNascimento;
-    @FXML
-    private TextField textPai;
-    @FXML
-    private TextField textMae;
-    @FXML
-    private TextField textRG;
+    private ToggleGroup sexoRadioGroup;
+
     @FXML
     private RadioButton radioMasculino;
+
     @FXML
     private RadioButton radioFeminino;
-
-    /**
-     * Initializes the controller class.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-
     @FXML
-    private void cadastrarDoador(ActionEvent event) {
-    }
-
+    private TableView<DoadorBD> tableView;
     @FXML
-    private void alterarDoador(ActionEvent event) {
-    }
-
+    private TableColumn<DoadorBD, String> tableColumnLogin;
     @FXML
-    private void excluirDoador(ActionEvent event) {
-    }
-
+    private TableColumn<DoadorBD, String> tableColumnNome;
     @FXML
-    private void cancelarCadastro(ActionEvent event) {
-    }
+    private TableColumn<DoadorBD, String> tableColumnEmail;
+    @FXML
+    private Button btnInserir;
     
+    private ObservableList<DoadorBD> observableDoadorBD;
+    
+    DoadorBD novodoador = new DoadorBD();
+    
+    
+    @FXML
+    void alterarDoador(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cadastrarDoador(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cancelarCadastro(ActionEvent event) {
+
+    }
+
+    @FXML
+    void excluirDoador(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void excluirTabela(ActionEvent event) {
+        int i = tableView.getSelectionModel().getSelectedIndex();
+        observableDoadorBD.remove(i);
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
+        alert.setTitle("Pedido Excluido");
+        alert.setHeaderText(null);
+        alert.setContentText("Pedido excluído com sucesso!");
+        alert.showAndWait();
+        tableView.refresh();
+    }
+
+    @FXML
+    private void alterarTabela(ActionEvent event) {
+    }
+
+    @FXML
+    private void inserirTabela(ActionEvent event) {
+        int i = 0;
+        novodoador.buscarIndividualBD(i);
+        novodoador.setLogin(/*login pego do banco*/);
+        novodoador.setNome(/*nome pego do banco*/);
+        novodoador.setEmail(/*email pego do banco*/);
+        observableDoadorBD.add(novodoador);
+        
+    }
+    public void initialize(URL location, ResourceBundle resources) {
+      observableDoadorBD = FXCollections.observableArrayList();      
+      tableColumnLogin.setCellValueFactory(new PropertyValueFactory<>("loginDoador"));
+      tableColumnNome.setCellValueFactory(new PropertyValueFactory<>("nomeDoador"));
+      tableColumnEmail.setCellValueFactory(new PropertyValueFactory<>("emailDoador"));
+      tableView.setItems(observableDoadorBD);
+    }
+
 }
