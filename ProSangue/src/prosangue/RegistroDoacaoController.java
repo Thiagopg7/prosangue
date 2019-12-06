73214ff664501cf9d60aa48c8cdd5a45f19824d1
Thiagopg7 <thiagopgpr@gmail.com>
2019-12-06 19:39:30 -0300

    Tela RegistroDoacao

--- ProSangue/src/prosangue/RegistroDoacaoController.java
+++ ProSangue/src/prosangue/RegistroDoacaoController.java
@@ -7,7 +7,12 @@ package prosangue;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -16,6 +21,33 @@ import javafx.fxml.Initializable;
  */
 public class RegistroDoacaoController implements Initializable {
 
+    @FXML
+    private TextField textCodigo;
+    @FXML
+    private Button buscaDoador;
+    @FXML
+    private CheckBox checkHepatiteB;
+    @FXML
+    private CheckBox checkHepatiteC;
+    @FXML
+    private CheckBox checkChagas;
+    @FXML
+    private CheckBox checkSifilis;
+    @FXML
+    private CheckBox checkAids;
+    @FXML
+    private CheckBox checkHtlv;
+    @FXML
+    private CheckBox checkAnemia;
+    @FXML
+    private CheckBox checkTriagemClinica;
+    @FXML
+    private Button btnCadastrarDoacao;
+    @FXML
+    private Button btnAlterarDoacao;
+    @FXML
+    private Button btnEnviarNotificacao;
+
     /**
      * Initializes the controller class.
      */
@@ -23,5 +55,9 @@ public class RegistroDoacaoController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void buscarDoador(ActionEvent event) {
+    }
     
 }
